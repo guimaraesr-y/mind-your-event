5076c0abcc88b9babacd17c9adf91c12a8c06c87
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-10-09 09:26:34 -0300

    Add cursor pointer

--- components/verify-email-form.tsx
+++ components/verify-email-form.tsx
@@ -115,7 +115,7 @@ export function VerifyEmailForm({
               </div>
             </div>
 
-            <Button type="submit" className="w-full" disabled={isLoading}>
+            <Button type="submit" className="cursor-pointer w-full" disabled={isLoading}>
               {isLoading ? (
                 <>
                   <Loader2 className="mr-2 h-4 w-4 animate-spin" />
