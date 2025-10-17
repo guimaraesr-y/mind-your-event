8bc055278d059145fdf518133e42b404918a69af
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-10-17 08:51:56 -0300

    Improve performance and add one more action

--- actions/event/retrieve.ts
+++ actions/event/retrieve.ts
@@ -1,13 +1,20 @@
 'use server';
 
 import { getSupabaseServerClient } from "@/lib/server";
+import { AvailabilitySlot } from "@/modules/events/availabilitySlot";
 import { EventInterface } from "@/modules/events/event";
+import { EventParticipant } from "@/modules/events/eventParticipants";
+import { UserInterface } from "@/modules/user/user";
 
-export async function retrieveEventById(eventId: string): Promise<EventInterface | null> {
+interface EventWithCreator extends EventInterface {
+    creator: Pick<UserInterface, "name" | "email">;
+}
+
+export async function retrieveEventById(eventId: string): Promise<EventWithCreator | null> {
     const supabase = await getSupabaseServerClient();
     const { data: event, error } = await supabase
         .from("events")
-        .select("*")
+        .select("*, users!events_creator_id_fkey(name, email)")
         .eq("id", eventId)
         .single();
 
@@ -18,7 +25,7 @@ export async function retrieveEventsByCreatorId(userId: string): Promise<EventIn
     const supabase = await getSupabaseServerClient();
     const { data: created } = await supabase
         .from("events")
-        .select("*, users!events_creator_id_fkey(name, email), event_participants!inner(*)")
+        .select("*, users!events_creator_id_fkey(name, email), event_participants(count)")
         .eq("creator_id", userId);
 
     return created || [];
@@ -34,7 +41,11 @@ export async function retrieveParticipatingEventsByUserId(userId: string): Promi
     return participating || [];
 }
 
-export async function retrieveEventParticipants(eventId: string): Promise<any[]> {
+interface EventParticipantWithUser extends EventParticipant {
+    user: Pick<UserInterface, "name" | "email">
+}
+
+export async function retrieveEventParticipants(eventId: string): Promise<EventParticipantWithUser[]> {
     const supabase = await getSupabaseServerClient();
     const { data: participants } = await supabase
         .from("event_participants")
@@ -42,4 +53,14 @@ export async function retrieveEventParticipants(eventId: string): Promise<any[]>
         .eq("event_id", eventId)
 
     return participants || [];
+}
+
+export async function retrieveEventAvailabilities(eventId: string): Promise<AvailabilitySlot[]> {
+    const supabase = await getSupabaseServerClient();
+    const { data: availabilitySlots } = await supabase
+        .from("availability_slots")
+        .select("*, users(name, email)")
+        .eq("event_id", eventId)
+
+    return availabilitySlots || [];
 }
