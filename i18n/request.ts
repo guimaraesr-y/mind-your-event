3bd3406139378d749e5f2d0af3625cc74eb8a40a
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-10-29 01:49:39 -0300

    Add translation check for api routes using header

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,11 @@
 import { hasLocale } from 'next-intl';
 import {getRequestConfig} from 'next-intl/server';
 import { routing } from './routing';
+import { headers } from 'next/headers';
  
 export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
+  const requested = await requestLocale || (await headers()).get('Accept-Language')?.split(',')[0];
+
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
