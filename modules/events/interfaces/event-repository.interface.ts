07877a1d3798c5eede6394d8bfb6343c9e5c1cc8
Ryan Guimarães <guimaraesryan26@gmail.com>
2026-02-02 19:19:07 -0300

    Add repository methods

--- modules/events/interfaces/event-repository.interface.ts
+++ modules/events/interfaces/event-repository.interface.ts
@@ -4,6 +4,7 @@ export interface IEventRepository {
     createEvent(eventData: Partial<EventInterface>): Promise<EventInterface>;
     updateEvent(eventId: string, eventData: Partial<EventInterface>): Promise<EventInterface>;
     getEventById(eventId: string): Promise<any | null>;
+    getEventByInviteToken(token: string): Promise<any | null>;
     getEventsByCreatorId(userId: string): Promise<any[]>;
     isEventOwner(userId: string, eventId: string): Promise<boolean>;
     isEventFinalized(eventId: string): Promise<boolean>;
--- modules/events/interfaces/participant-repository.interface.ts
+++ modules/events/interfaces/participant-repository.interface.ts
@@ -1,6 +1,8 @@
 export interface IParticipantRepository {
-    createParticipant(eventId: string, userId: string, inviteToken: string): Promise<void>;
+    createParticipant(eventId: string, userId: string, inviteToken: string): Promise<any>;
     getParticipantsByEventId(eventId: string): Promise<any[]>;
+    getParticipantByInviteToken(token: string): Promise<any | null>;
+    getParticipantByEventAndUser(eventId: string, userId: string): Promise<any | null>;
     updateParticipantStatus(eventId: string, userId: string, hasSubmitted: boolean): Promise<void>;
     updateRsvp(eventId: string, inviteToken: string, willAttend: boolean): Promise<void>;
 }
