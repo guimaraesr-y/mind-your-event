3558c65330984206818c4763f4ddf1af54549ac7
Ryan Guimarães <guimaraesryan26@gmail.com>
2026-02-11 20:16:11 -0300

    Add HELLA BEAUTIFUL 404 page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "MindYourEvent",
@@ -10,5 +22,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
+
